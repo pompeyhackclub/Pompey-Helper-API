1ee62aefe2aed43a616e46f53e25d880371d251f
Michael Parker <michaelrbparker@protonmail.com>
2026-05-10 11:53:23 +0100

    db gen

---
+++ init_db.py
@@ -0,0 +1,33 @@
+import aiosqlite
+
+async def init_db():
+    async with aiosqlite.connect('database.db') as db:
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                uuid TEXT NOT NULL UNIQUE,
+                username TEXT NOT NULL UNIQUE,
+                email TEXT NOT NULL UNIQUE,
+                passwordHash TEXT NOT NULL,
+                RewardPoints INTEGER NOT NULL DEFAULT 0
+            )
+        ''')
+
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS Tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                userId INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                description TEXT,
+                completed BOOLEAN NOT NULL DEFAULT FALSE,
+                Location TEXT,
+                FOREIGN KEY (userId) REFERENCES users (id)
+            )
+        ''')
+        await db.commit()
+
+        
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(init_db())
